38e8176d63baa76f5853db669992da64e0637e97
OrestesFerraz <148365065+OrestesFerraz@users.noreply.github.com>
2024-03-05 16:43:44 -0300

    Add files via upload

---
+++ Eder/fruta/banana.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <marquee behavior="loop" direction="left">
+        banana
+    </marquee>
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQv6U9wHR4Ign5ZhLJWEX2xd5bkCf9RSXImPAlkp_COYXb3G5BdKgpmeDKtlL1HZFWjL48&usqp=CAU" alt="banana">
+</body>
+</html>
